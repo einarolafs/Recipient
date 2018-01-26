b1203ce82faee3a215b4a7d8013e0c24ede0f800
Einar Ólafsson <einsiol@gmail.com>
2018-01-26 17:53:32 +0100

    Fixing a CORS issue when trying to post from client to server

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const port = process.env.PORT || 5000;
 const countries = [{id: "germany", name: "Germany"}, {id: "netherlands", name: "Netherlands"}];
 let tasks = [];
